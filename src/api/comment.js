01bb96f969fef6a0fb9203a38deff0997829e9e5
weiyuwen <3454784839@qq.com>
2022-11-04 13:57:58 +0800

    comment

--- src/api/comment.js
+++ src/api/comment.js
@@ -1,11 +1,5 @@
 import request from '@/utils/request'
 export default{
-    getCommentByUserId(userId){
-        return request({
-            url:`/comments/selectByUserId/${userId}`,
-            method:'get'
-        })
-    },
     getCommentByproId(proId){
         return request({
             url:`/comments/selectbyproId/${proId}`,
