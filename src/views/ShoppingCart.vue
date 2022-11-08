39ee89f19741458568f0f33913e163bb6e9d9e1a
zzZZ <947091759@qq.com>
2022-11-08 14:00:54 +0800

    购物车传用户优惠券id

--- src/views/ShoppingCart.vue
+++ src/views/ShoppingCart.vue
@@ -229,7 +229,7 @@ function choosexiaogou(coupon){
         }
         else {
             c.isChecked = false
-            couponCheckedId = []
+            couponCheckedId = c.receiveId
         }
     })
 }
